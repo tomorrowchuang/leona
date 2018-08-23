28f6fef7d8c301e21ea38377ebc76bee29eb0118
Lac <laclys@126.com>
2018-08-23 14:18:36 +0800

    add cloudfunction config

--- config.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Lac 
- * @Date: 2018-08-22 22:35:23 
- * @Last Modified by:   Lac 
- * @Last Modified time: 2018-08-22 22:35:23 
- */
-export const config = {
-  api_base_url: 'xxxx'
-}
---
+++ miniprogram/app.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Lac 
+ * @Date: 2018-08-23 14:13:13 
+ * @Last Modified by:   Lac 
+ * @Last Modified time: 2018-08-23 14:13:13 
+ */
+
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init()
+    }
+
+    this.globalData = {}
+  }
+})
