e88cb9e5ab88284a7f3fd52b50973469f9762412
Lac <laclys@126.com>
2018-08-24 14:50:15 +0800

    feat: nav component completed

--- miniprogram/components/nav/index.js
+++ miniprogram/components/nav/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Lac 
  * @Date: 2018-08-24 11:56:35 
- * @Last Modified by:   Lac 
- * @Last Modified time: 2018-08-24 11:56:35 
+ * @Last Modified by: Lac
+ * @Last Modified time: 2018-08-24 14:46:35
  */
 
 Component({
@@ -10,7 +10,8 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    isFirst: Boolean,
+    isLatest: Boolean
   },
 
   /**
@@ -24,6 +25,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    handlePreClick: function (ev) {
+      if (!this.properties.isFirst) this.triggerEvent('pre', {})
+    },
 
+    handleNextClick: function (ev) {
+      if (!this.properties.isLatest) this.triggerEvent('next', {})
+    }
   }
 })
