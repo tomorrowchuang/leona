b835a09324f7456de0f7fb4b4ec93e28a686df1a
Lac <laclys@126.com>
2018-08-24 11:25:41 +0800

    chore: update wx-cloud function && episode model update

--- miniprogram/models/episode.js
+++ miniprogram/models/episode.js
@@ -2,25 +2,19 @@
  * @Author: Lac 
  * @Date: 2018-08-22 22:36:56 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-08-23 16:07:08
+ * @Last Modified time: 2018-08-24 11:22:15
  */
 import { WXCloud } from '../utils/wx-cloud.js'
 
  
 export class EpisodeModel extends WXCloud {
   getLatest(callback) {
-    this.getCount({
-      collection: 'episode',
-      query: {},
-      success: index => {
-        this.getData({
-          collection: 'episode',
-          query: { index: index},
-          success: res => {
-            callback(res)
-          }
-        })
-      }
-    })
+      this.getData({
+        collection: 'episode',
+        limit: 1,
+        success: res => {
+          callback(res)
+        }
+      })
   }
 }
--- miniprogram/utils/wx-cloud.js
+++ miniprogram/utils/wx-cloud.js
@@ -2,26 +2,31 @@
  * @Author: Lac 
  * @Date: 2018-08-23 15:18:50 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-08-23 15:45:14
+ * @Last Modified time: 2018-08-24 11:24:29
  */
 import { fetchMsg } from "../const/const.js";
  
 export class WXCloud {
   /**
    * Query.get
-   * @param { collection, query, success } params 
+   * @param { collection, query, success, limit } params 
    */
   getData(params) {
-    params.query = params.query ? params.query : {}
+    let query = params.query ? params.query : {}
+    let limit = params.limit ? params.limit : 20
     const db = wx.cloud.database()
-    db.collection(params.collection).where(params.query).get({
-      success: function(res) {
-        params.success && params.success(res.data)
-      },
-      fail: (err) => {
-        this._show_error()
-      }
-    })
+    db.collection(params.collection)
+      .where(query)
+      .limit(limit)
+      .orderBy('index', 'desc')
+      .get({
+        success: function(res) {
+          params.success && params.success(res.data)
+        },
+        fail: (err) => {
+          this._show_error()
+        }
+      })
   }
 
   /**
@@ -29,16 +34,18 @@ export class WXCloud {
    * @param { collection, query, success } params 
    */
   getCount(params) {
-    params.query = params.query ? params.query : {}
+    let query = params.query ? params.query : {}
     const db = wx.cloud.database()
-    db.collection(params.collection).where(params.query).count({
-      success: function(res) {
-        params.success && params.success(res.total)
-      },
-      fail: (err) => {
-        this._show_error()
-      }
-    })
+    db.collection(params.collection)
+      .where(query)
+      .count({
+        success: function(res) {
+          params.success && params.success(res.total)
+        },
+        fail: (err) => {
+          this._show_error()
+        }
+      })
   }
 
   _show_error() {
