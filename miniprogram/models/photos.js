6987b8273c77165e96e444fe7970a3dd06441b3c
Lac <laclys@126.com>
2018-08-28 23:25:10 +0800

    feat: add PhotoModel

---
+++ miniprogram/models/photos.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: Lac 
+ * @Date: 2018-08-28 23:05:56 
+ * @Last Modified by: Lac
+ * @Last Modified time: 2018-08-28 23:24:35
+ */
+
+import { WXCloud } from '../utils/wx-cloud.js'
+
+export class PhotoModel extends WXCloud {
+  getPhotos(index, callback) {
+    let searchIndex = Number(index)
+    this.getData({
+      collection: 'photos',
+      query: {
+        index: searchIndex
+      },
+      success: res => {
+        let result = res[0]
+        callback(result)
+      }
+    })
+  }
+}
