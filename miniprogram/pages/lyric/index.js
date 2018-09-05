aaab747cc28702dbaea492b8429679cee406d8e0
Lac <laclys@126.com>
2018-09-05 22:23:01 +0800

    feat: get song Model data

--- miniprogram/pages/lyric/index.js
+++ miniprogram/pages/lyric/index.js
@@ -1,24 +1,35 @@
 /*
  * @Author: Lac 
  * @Date: 2018-09-05 16:48:52 
- * @Last Modified by:   Lac 
- * @Last Modified time: 2018-09-05 16:48:52 
+ * @Last Modified by: Lac
+ * @Last Modified time: 2018-09-05 22:11:19
  */
+import { SongModel } from '../../models/song'
+import { DEFAULT, PENDING, SUCCESS, FAIL } from '../../const/async-status'
+import { errorMsg } from '../../const/const'
+import navToggleMixin from '../../utils/nav-toggle-mixin'
+import mergePage from '../../utils/merge-page'
 
-Page({
+let songModel = new SongModel()
+
+Page(mergePage(navToggleMixin(['', '歌詞']) ,{
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    content: '',
+    title: '',
+    status: DEFAULT,
+    errorMsg: errorMsg
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { index } = options
+    this._fetchData(index)
   },
 
   /**
@@ -68,5 +79,16 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  _fetchData: function (index) {
+    this.setData({
+      status: PENDING
+    })
+    wx.nextTick(() => {
+      songModel.getLyric(index, res => {
+        console.log(res)
+      })
+    })
   }
-})
+}))
