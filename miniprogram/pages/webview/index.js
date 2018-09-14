e0162ec28ae6c740c563d312174709c505a8b7aa
Lac <laclys@126.com>
2018-09-14 17:42:21 +0800

    feat: add webview common page

---
+++ miniprogram/pages/webview/index.js
@@ -0,0 +1,75 @@
+/*
+ * @Author: Lac 
+ * @Date: 2018-09-14 17:40:11 
+ * @Last Modified by: Lac
+ * @Last Modified time: 2018-09-14 17:40:32
+ */
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    url: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let { url } = options
+    this.setData({
+      url
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
