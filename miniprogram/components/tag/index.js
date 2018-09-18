a49873d0f596016cd6419ceffe1d3fa6d9dffe33
Lac <laclys@126.com>
2018-09-18 21:51:58 +0800

    feat: add tag comp

---
+++ miniprogram/components/tag/index.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: Lac
+ * @Date: 2018-09-02 16:20:00
+ * @Last Modified by: Lac
+ * @Last Modified time: 2018-09-18 21:50:03
+ */
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  options: {
+    multipleSlots: true
+  },
+
+  externalClasses: ['tag-class'],
+
+  properties: {
+    text: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
