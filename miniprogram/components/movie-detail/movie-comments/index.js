2ff93ea830b24caf6e3e1fc1200e82465e2d4c5e
Lac <laclys@126.com>
2018-09-19 12:31:34 +0800

    init movie-comments comp

---
+++ miniprogram/components/movie-detail/movie-comments/index.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: Lac 
+ * @Date: 2018-09-19 12:30:43 
+ * @Last Modified by:   Lac 
+ * @Last Modified time: 2018-09-19 12:30:43 
+ */
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    data: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
